b5a1f83689f2ab0c24c76cad5026acd471f6c060
ikbol009@gmail.com <ikbol009@gmail.com>
2018-03-10 12:50:34 -0500

    added Priver

--- src/main/GoodEvening.java
+++ src/main/GoodEvening.java
@@ -5,5 +5,9 @@ public class GoodEvening {
 	public static void main(String[] args) {
 		System.out.println("Good Evening class!");
 		System.out.println("12345");
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Priver Mikhailo");
+		}
 	}
 }
