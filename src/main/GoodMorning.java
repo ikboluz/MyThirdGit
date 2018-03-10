671bd4ee2f374670c17866b6fe904d2dc19b7e82
ikbol009@gmail.com <ikbol009@gmail.com>
2018-03-10 12:51:55 -0500

    sava sava

--- src/main/GoodMorning.java
+++ src/main/GoodMorning.java
@@ -4,5 +4,9 @@ public class GoodMorning {
 
 	public static void main(String[] args) {
 		System.out.println("Good Morning class!");
+		
+		for (int i = 0; i < 10; i++) {
+			System.out.println("Solut sava sava Mikhailo");
+		}
 	}
 }
